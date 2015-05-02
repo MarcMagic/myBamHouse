09af6f10823e7b42e7b902164468ed0e8212d29a
MarcMagic <marcmo@gmx.net>
2015-05-02 19:39:19 +0200

    new title

--- index.php
+++ index.php
@@ -1,10 +1,10 @@
 <!DOCTYPE HTML>
 <html>
 	<head>
-		<title>BamHouse</title>
+		<title>BamHouse - virale Trends</title>
 		<meta http-equiv="content-type" content="text/html; charset=UTF-8">
 		<meta name="description" content="Informationen rund um die deutsche Internetcommunity">
- 		<meta name="keywords" content="Internet, YouTube, Twitter, Facebook, Hashtag">
+ 		<meta name="keywords" content="Internet, YouTube, Twitter, Facebook, Hashtag, viral, trends, online, instagram, news, blogger, bamhouse">
 		<link rel="icon" href="http://www.bamhouse.de/images/favicon.ico" type="image/x-icon" />
 		<link href="css/styles.css" rel="stylesheet" type="text/css" media="screen">
 		<link href='http://fonts.googleapis.com/css?family=PT+Sans+Narrow:400,700' rel='stylesheet' type='text/css'>
