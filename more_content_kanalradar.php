51af4d8d74eb508f3bf9a4af02f5e43e0afc8f27
Mareike <mareike.haug@web.de>
2015-05-10 04:11:08 +0200

    php in

---
+++ more_content_kanalradar.php
@@ -0,0 +1,40 @@
+<?php
+include "connect.php";
+mysql_query("SET NAMES 'utf8'");
+if(isSet($_POST['getLastContentId'])) {
+	$getLastContentId=$_POST['getLastContentId'];
+	$result=mysql_query("SELECT * FROM `index` WHERE id <'$getLastContentId' ORDER BY id DESC LIMIT 10");
+	$count=mysql_num_rows($result);
+	if($count>0){
+		while ($row = mysql_fetch_assoc($query)) {
+							$id = $row['id'];
+							$kanallink = $row['kanallink'];
+							$kanalname = $row['kanalname'];
+							$beschreibung = $row['beschreibung'];
+			?>				
+			<div class="kanalprofil">
+				
+				<div class="kanalpic">
+					<?php echo "<img src='{$kanalname}.jpg' alt='{$kanalname}' title='{$kanalname}'";?>
+				</div>
+				
+				<div class="kanaltext">
+					<?php echo "<h1><a href ='{$kanallink}'>{$kanalname}"; ?></a></h1>
+					<p><?php echo "{$beschreibung}";?></p>
+				</div>		
+				
+			</div>;
+		}
+		?>
+ 
+		<a href="#"><div id="load_more_<?php echo $id; ?>" class="more_tab">
+		<div id="<?php echo $id; ?>" class="more_button">Mehr Kanäle laden</div></a>
+		</div>
+ 
+	<?php
+	}
+	else{
+		echo "<div class='all_loaded'>Es gibt keine Kanäle mehr</div>";
+	}
+}
+?>
